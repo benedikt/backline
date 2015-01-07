a045ede3b876d9bffdd386b68e34d91e8a49320d
Benedikt Deicke <benedikt@synatic.net>
2015-01-07 14:52:05 +0100

    Implemeints loading models from the repository

--- lib/backline/repository.rb
+++ lib/backline/repository.rb
@@ -1,22 +1,62 @@
 module Backline
   class Repository
 
-    def self.create(path)
+    def self.create(path, &block)
       Rugged::Repository.init_at(path, :bare)
 
-      new(path)
+      new(path, &block)
     end
 
-    def self.clone(remote_path, local_path)
+    def self.clone(remote_path, local_path, &block)
       Rugged::Repository.clone_at(remote_path, local_path, bare: true)
 
-      new(local_path)
+      new(local_path, &block)
     end
 
     def initialize(path)
       @repository = Rugged::Repository.new(path)
+      yield self if block_given?
     rescue Rugged::OSError => e
       raise(Backline::Error, e.message)
+    end
+
+    def register(type, path = nil)
+      mapping[type] = (path || type.name.underscore).to_s
+    end
+
+    def find(type, path)
+      tree = subtree_for(type)
+      oid = tree.path(path.to_s)[:oid]
+      load_from_oid(type, oid)
+    end
+
+    def all(type)
+      tree = subtree_for(type)
+      tree.enum_for(:each_blob).map do |entry|
+        load_from_oid(type, entry[:oid])
+      end
+    end
+
+  private
+
+    attr_reader :repository
+
+    def mapping
+      @mapping ||= {}
+    end
+
+    def tree
+      repository.head.target.tree
+    end
+
+    def load_from_oid(type, oid)
+      content = repository.lookup(oid).content
+      type.load(content)
+    end
+
+    def subtree_for(type)
+      oid = tree.path(mapping[type])[:oid]
+      repository.lookup(oid)
     end
 
   end
--- spec/backline/repository_spec.rb
+++ spec/backline/repository_spec.rb
@@ -18,6 +18,11 @@ describe Backline::Repository do
       expect { expect(described_class.new('unknown-repository')) }
         .to raise_error(Backline::Error, /No such file or directory/)
     end
+
+    it 'should yield itself' do
+      expect { |b| described_class.new(repository_path, &b) }
+        .to yield_with_args(described_class)
+    end
   end
 
   describe '.create' do
@@ -29,6 +34,11 @@ describe Backline::Repository do
     it 'should open the created repository' do
       expect(described_class.create(repository_path))
         .to be_instance_of(described_class)
+    end
+
+    it 'should yield itself' do
+      expect { |b| described_class.create(repository_path, &b) }
+        .to yield_with_args(described_class)
     end
   end
 
@@ -47,6 +57,62 @@ describe Backline::Repository do
       expect(described_class.clone(remote_path, repository_path))
         .to be_instance_of(described_class)
     end
+
+    it 'should yield itself' do
+      expect { |b| described_class.clone(remote_path, repository_path, &b) }
+        .to yield_with_args(described_class)
+    end
   end
 
+  context 'persistence' do
+    let(:dummy_class) do
+      Class.new do
+        include Backline::Model
+
+        def self.name
+          'Dummy'
+        end
+
+        attribute :foo
+        attribute :bar
+      end
+    end
+
+    subject do
+      described_class.new(repository_path) do |r|
+        r.register dummy_class, 'dummy'
+      end
+    end
+
+    before do
+      repository = Rugged::Repository.init_at(repository_path)
+
+      index = repository.index
+      index.add(path: 'dummy/1', oid: repository.write("---\nfoo: foo\nbar: bar\n", :blob), mode: 0100644)
+      index.add(path: 'dummy/2', oid: repository.write("---\nfoo: foo\nbar: bar\n", :blob), mode: 0100644)
+
+      options = {
+        tree: index.write_tree(repository),
+        message: 'Initial commit',
+        parents: [],
+        update_ref: 'HEAD'
+      }
+
+      Rugged::Commit.create(repository, options)
+    end
+
+    describe '#find' do
+      it 'should find the record' do
+        expect(subject.find(dummy_class, 1))
+          .to be_kind_of(dummy_class)
+      end
+    end
+
+    describe '#all' do
+      it 'should find the records' do
+        expect(subject.all(dummy_class))
+          .to match([an_instance_of(dummy_class), an_instance_of(dummy_class)])
+      end
+    end
+  end
 end
